f636a962ad80f51c354c820ba8555ef2bb9853e4
rahul02shah <rahul02.shah@outlook.com>
2023-07-21 21:01:51 +0545

    combining array

---
+++ combining_array/Concat_method.js
@@ -0,0 +1,6 @@
+// combining array using concat method 
+let array1 = [1,2,3];
+let array2 = [4,5,6];
+
+let array = array1.concat(array2);
+console.log(arrayyy);
---
+++ combining_array/Spread_Operator.js
@@ -0,0 +1,6 @@
+// combining array using spread operator
+let array1 = [1,2,3];
+let array2 = [4,5,6];
+let array = [...array1,...array2];
+console.log(array);
+
