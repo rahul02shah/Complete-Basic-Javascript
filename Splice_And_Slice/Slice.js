4fc0b549b1193e77d88fe61123b1fda8bf84ec3c
rahul02shah <rahul02.shah@outlook.com>
2023-07-21 21:23:17 +0545

    splice and slice method

---
+++ Splice_And_Slice/Slice.js
@@ -0,0 +1,8 @@
+// using slice we can only return the delete element
+
+let array = [1,2,6,7,90];
+
+;
+console.log(array.slice(2,3));
+
+ // output : [ 6, 7, 90 ]
---
+++ Splice_And_Slice/Splice.js
@@ -0,0 +1,8 @@
+// using splice method we can delete and insert elemrnt
+let array = [1,2,4,6,8];
+array.splice(3,0,99);
+console.log(array);
+
+// output : [ 1, 2, 4, 99, 6, 8 ]
+
+
