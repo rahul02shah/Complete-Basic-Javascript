5eea0ac81a93fd1c656a20701088f04f82ad4f89
rahul02shah <rahul02.shah@outlook.com>
2023-07-22 17:31:00 +0545

    try catch in js

--- Try_Catch.js
+++ Try_Catch.js
@@ -3,3 +3,16 @@ try{
 }catch(e){
     console.log("function not defined");
 }
+
+
+// custom Error Handeling 
+function sum(a,b){
+    if((a && b)!== Number){
+        throw new Error("Entered string")
+    }
+}
+try{
+    sum(3,"rahul");
+}catch(e){
+    console.log(e);
+}
