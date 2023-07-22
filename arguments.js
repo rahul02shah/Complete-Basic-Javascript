99b8d34ba7e0358e57791c2ba2fa344074f484c5
rahul02shah <rahul02.shah@outlook.com>
2023-07-22 15:41:21 +0545

    argument in js

--- arguments.js
+++ arguments.js
@@ -1,6 +1,10 @@
 function sum(a,b){
-    console.log(arguments);
-    console.log(arguments['3']);
+    let total = 0;
+    for(value in arguments){
+        total = total + arguments[value];
+    }
+    return total;
 }
 
-let Result = sum(6,7,8,9)
+let Result = sum(6,7,8,9);
+console.log(Result);
